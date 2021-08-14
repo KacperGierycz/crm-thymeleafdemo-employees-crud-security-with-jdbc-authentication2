2a7a68b8c1d6974538b206dd993b168ade71a5f4
Kacper <kacpergierycz@gmail.com>
2021-08-14 11:32:19 +0200

    Project Working on AWS

---
+++ thymeleafdemo-employees-crud-security-with-jdbc-authenticationv2/src/main/java/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -0,0 +1,151 @@
+package springboot.thymeleafdemo.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+import springboot.thymeleafdemo.entity.Employee;
+import springboot.thymeleafdemo.service.EmployeeService;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+	private EmployeeService employeeService;
+	
+	public EmployeeController(EmployeeService theEmployeeService) {
+		employeeService = theEmployeeService;
+	}
+	
+	// initial binder to preprocess every web request and trim Strings if whitespaces
+	
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+		
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+		
+		dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+	}	
+	
+	
+	// add mapping for "/list"
+
+	@GetMapping("/list")
+	public String listEmployees(Model theModel) {
+		
+		// get employees from db
+		List<Employee> theEmployees = employeeService.findAll();
+		
+		// add to the spring model
+		theModel.addAttribute("employees", theEmployees);
+		
+		return "employees/list-employees";
+	}
+	
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model theModel) {
+		
+		// create model attribute to bind form data
+		Employee theEmployee = new Employee();
+		
+		theModel.addAttribute("employee", theEmployee);
+		
+		return "employees/employee-form";
+	}
+
+	
+	
+	
+	@GetMapping("/showFormForUpdate")
+	public String showFormForUpdate( @RequestParam("employeeId") int theId,
+									Model theModel) {
+		
+		// get the employee from the service
+		Employee theEmployee = employeeService.findById(theId);
+		
+		// set employee as a model attribute to pre-populate the form
+		theModel.addAttribute("employee", theEmployee);
+		
+		// send over to our form
+		return "employees/employee-form";			
+	}
+	
+
+	
+	// validation
+	@PostMapping("/save")
+	public String saveEmployee(@Valid @ModelAttribute("employee") Employee theEmployee, BindingResult theBindingResult) {
+		
+		System.out.println(1);
+		// form validation
+		 if (theBindingResult.hasErrors()){
+				System.out.println(2);
+			 return "/employees/employee-form";
+	        }
+		
+		// save the employee
+		employeeService.save(theEmployee);
+		
+		// use a redirect to prevent duplicate submissions
+		return "redirect:/employees/list";
+	}
+	
+	
+	@GetMapping("/delete")
+	public String delete(@RequestParam("employeeId") int theId) {
+		
+		// delete the employee
+		employeeService.deleteById(theId);
+		
+		// redirect to /employees/list
+		return "redirect:/employees/list";
+		
+	}
+	
+	@GetMapping("/search")
+	public String delete(@RequestParam("employeeName") String theName,
+						 Model theModel) {
+		
+		// delete the employee
+		List<Employee> theEmployees = employeeService.searchBy(theName);
+		
+		// add to the spring model
+		theModel.addAttribute("employees", theEmployees);
+		
+		// send to /employees/list
+		return "employees/list-employees";
+		
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ thymeleafdemo-employees-crud-security-with-jdbc-authenticationv2/src/main/java/springboot/thymeleafdemo/service/EmployeeService.java
@@ -0,0 +1,19 @@
+package springboot.thymeleafdemo.service;
+
+import java.util.List;
+
+import springboot.thymeleafdemo.entity.Employee;
+
+public interface EmployeeService {
+
+	public List<Employee> findAll();
+	
+	public Employee findById(int theId);
+	
+	public void save(Employee theEmployee);
+	
+	public void deleteById(int theId);
+
+	public List<Employee> searchBy(String theName);
+	
+}
